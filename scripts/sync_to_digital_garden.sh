5f94278d86da4aa72b5178df577069797fc471f5
Maneesh Sutar <sutarmaneesh00@gmail.com>
2024-10-05 15:02:37 +0530

    Update script to check if local notes repo is a git repo

--- scripts/sync_to_digital_garden.sh
+++ scripts/sync_to_digital_garden.sh
@@ -15,16 +15,46 @@ then
 	read 
 fi
 
-START_AT=$1
-OBSIDIAN_ROOT=$PWD
-DIGITAL_GARDEN_ROOT=$HOME/opensource/digital-garden-notes
+START_AT="$1"
+OBSIDIAN_ROOT="$PWD"
+DIGITAL_GARDEN_ROOT="$HOME/opensource/digital-garden-notes"
 
-if [[ ! -d $DIGITAL_GARDEN_ROOT ]]
-then
-	echo "ERROR: Please change your digital garden repo location in the script"
-	echo "Make sure it is cloned from your github repo"
-	exit 1
-fi
+check_git_repo() {
+    local folder_path="$1"
+
+    # Check if the folder exists
+    if [[ ! -d "$folder_path" ]]; then
+        echo "ERROR: Please update your digital garden repo location in the script"
+	    echo "Make sure it is cloned from your github repo"
+        exit 1
+    fi
+
+    # Use a subshell to avoid affecting the caller's working directory
+    (
+        cd "$folder_path" || exit 1
+
+        # Check if the folder is a git repository
+        if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+            echo "Error: '$folder_path' is not a Git repository."
+            exit 1
+        fi
+
+        # Check for changes in the working directory
+        if [[ -n "$(git status --porcelain)" ]]; then
+            echo "Error: There are uncommitted changes in the working directory."
+            exit 1
+        fi
+    )
+
+    if [[ $? -eq 0 ]]; then
+        echo "No uncommitted changes and it's a valid Git repository."
+    else
+        exit 1
+    fi
+}
+
+check_git_repo "$DIGITAL_GARDEN_ROOT"
+
 
 conditional_delete() {
   local dir_path=$1
@@ -70,4 +100,3 @@ else # only export subfolder
 	obsidian-export --hard-linebreaks --skip-tags private --start-at $OBSIDIAN_ROOT/$START_AT $OBSIDIAN_ROOT $OUTPUT_PATH
 	echo "Notes have been exported to $OUTPUT_PATH"
 fi
-
